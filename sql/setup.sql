79536c0aad19772aa8e7bce0026abc994a8b1b5f
Dillon Brock <mdillon1297@gmail.com>
2022-07-27 14:44:47 -0700

    added SQL to generate database

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists plants;
+
+CREATE table plants (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    latin_name VARCHAR NOT NULL,
+    water_need VARCHAR NOT NULL,
+    light_need VARCHAR NOT NULL,
+    indoor BOOLEAN
+);
+
+INSERT INTO plants (name, latin_name, water_need, light_need, indoor) VALUES
+('Spider Plant', 'Chlorophytum comosum', 'once a week', 'indirect sun', true),
+('ZZ Plant', 'Zamioculcas', 'every 2-3 weeks', 'indirect sun', true),
+('Heliotrope', 'Heliotropium arborescens', 'keep soil moist', 'direct sun', false),
+('Sunflower', 'Helianthus', 'once a week', 'direct sun', false);
